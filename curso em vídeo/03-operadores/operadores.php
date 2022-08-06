fad9e74c0f805920624ee3e187596e4a1f812f7b
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-06 17:44:08 -0300

    Create operadores.php

---
+++ curso em vídeo/03-operadores/operadores.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usando operadores.</title>
+</head>
+<body>
+    <?php
+    $n1 = 5;
+    $n2 = 10.25;
+    $soma = $n1 + $n2;
+
+    echo "A soma entre $n1 e $n2 é igual a $soma";
+
+    ?>
+    
+</body>
+</html>
