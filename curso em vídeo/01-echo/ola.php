72a7390234617aa728bf0a2e22be5085d9e1ddd4
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-06 17:36:22 -0300

    Create ola.php

---
+++ curso em vídeo/01-echo/ola.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <style>
+        h2{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+<h1>Meu primeiro programa em PHP</h1>
+<?php                                   /* super tag, para incluir códigos em PHP*/
+echo "<h2> olá mundo</h2>";         /* 'echo' mostra algo na tela. Também pode ser usado 'print'*/
+print "<p>este é um parágrafo!</p>"
+?>
+
+</body>
+</html>
