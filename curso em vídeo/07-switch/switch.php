c829c3d4dd1199fc0c11946f293e8934d949b9c8
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-06 22:50:50 -0300

    Create switch.php

---
+++ curso em vídeo/07-switch/switch.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis em PHP</title>
+</head>
+<body>
+    <?php   
+        $idade = -25;
+
+        switch($idade){
+
+            case($idade >= 0 && $idade < 18):
+                echo "Menor de idade!";
+                break;
+            
+            case($idade >= 18 && $idade < 60):
+                echo "Adulto!";
+                break;
+              
+            case($idade >= 60 && $idade < 130):
+                echo "Idoso!";
+                break;
+
+            default:
+                echo "Idade incorreta!";
+
+        }
+
+?>
+    
+</body>
+</html>
