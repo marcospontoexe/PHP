dd99600971c0c34a755c36744931c0463eafc4e4
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-10-02 22:32:33 -0300

    visibilidade de atributos e metodos

--- curso em vídeo/15-objetos/01-Classes/Caneta.php
+++ curso em vídeo/15-objetos/01-Classes/Caneta.php
@@ -2,20 +2,61 @@
 
 class Caneta {
     //atributos
-    var $modelo;
-    var $cor;
-    var $ponta;
-    var $carga;
-    var $tampada;    
+    private $modelo;
+    private $cor;
+    private $ponta;
+    private $carga;
+    private $tampada;    
     
     //métodos
-    function rabiscar(){
+    public function rabiscar(){
         echo '<p>Estou rabiscando...</p>';
     }
-    function tampar(){
+    public function tampar(){
         $this->tampada = true;
     }
-    function destampar(){
+    public function destampar(){
         $this->tampada = false;
     }
+    public function getModelo() {
+        return $this->modelo;
+    }
+
+    public function getCor() {
+        return $this->cor;
+    }
+
+    public function getPonta() {
+        return $this->ponta;
+    }
+
+    public function getCarga() {
+        return $this->carga;
+    }
+
+    public function getTampada() {
+        return $this->tampada;
+    }
+
+    public function setModelo($modelo): void {
+        $this->modelo = $modelo;
+    }
+
+    public function setCor($cor): void {
+        $this->cor = $cor;
+    }
+
+    public function setPonta($ponta): void {
+        $this->ponta = $ponta;
+    }
+
+    public function setCarga($carga): void {
+        $this->carga = $carga;
+    }
+
+    public function setTampada($tampada): void {
+        $this->tampada = $tampada;
+    }
+
+
 }
--- curso em vídeo/15-objetos/01-Classes/index.php
+++ curso em vídeo/15-objetos/01-Classes/index.php
@@ -6,21 +6,23 @@
         <title>Criando Classes</title>
     </head>
     <body>
-        <?php
-            require_once 'Caneta.php';  //importando a classe caneta
-            $c1 = new Caneta();         //instanciando um objeto
-            
-            $c1->cor = "preta";
-            $c1->ponta = 0.5;
-            $c1->modelo = "bic";
-            $c1->tampada = false;
-            
-            print_r($c1);       //mostra o estado do objeto c1
-            
-            $c1->rabiscar();
-            $c1->tampar();
-            print_r($c1); 
-            
-        ?>
+        <pre>
+            <?php
+                require_once 'Caneta.php';  //importando a classe caneta
+                $c1 = new Caneta();         //instanciando um objeto
+
+                $c1->setCor("preta");
+                $c1->setPonta(0.5);
+                $c1->setModelo("bic");
+                $c1->setTampada(false);
+
+                print_r($c1);       //mostra o estado do objeto c1
+
+                $c1->rabiscar();
+                $c1->tampar();
+                print_r($c1); 
+
+            ?>
+        </pre>
     </body>
 </html>
